dcd3c68d850ad4dcecc2846ab04ce33b9311742b
Lee JaeHyun <myleje@gmail.com>
2026-07-22 15:54:54 +0900

    Update CannonController.cs

--- Assets/02_Scripts/Tank/CannonController.cs
+++ Assets/02_Scripts/Tank/CannonController.cs
@@ -8,7 +8,7 @@ public class CannonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float angle = _mouseWheel.action.ReadValue<float>() * Time.deltaTime * 10.0f;
+        float angle = _mouseWheel.action.ReadValue<float>() * Time.deltaTime * 200.0f;
 
         transform.Rotate(Vector3.right * angle);
     }
