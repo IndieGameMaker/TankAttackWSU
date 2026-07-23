cff4a5a5b939aa7c15c2eee8ef24ce369d156b58
Lee JaeHyun <myleje@gmail.com>
2026-07-23 16:33:58 +0900

    .

--- Assets/02_Scripts/Manager/GameManager.cs
+++ Assets/02_Scripts/Manager/GameManager.cs
@@ -13,6 +13,13 @@ public class GameManager : MonoBehaviourPunCallbacks
 
     private PhotonView _pv;
 
+    public override void OnEnable()
+    {
+        base.OnEnable();
+
+        chatInput.onEndEdit.AddListener( _ => OnSendMessage());
+    }
+
     private void Start()
     {
         _pv = GetComponent<PhotonView>();
@@ -42,10 +49,12 @@ public class GameManager : MonoBehaviourPunCallbacks
         DisplayRoomInfo();
     }
 
-    private void MessageSend()
+    private void OnSendMessage()
     {
         // Message
         string msg = $"<color=green>[{PhotonNetwork.NickName}]</color> {chatInput.text}";
+        _pv.RPC(nameof(ChatMessage), RpcTarget.AllBufferedViaServer, msg);
+        chatInput.text = "";
     }
 
     [PunRPC]
