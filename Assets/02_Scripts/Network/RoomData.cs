afcd3fe87d122096b203af1d4f4d611f8f82bb41
Lee JaeHyun <myleje@gmail.com>
2026-07-24 16:24:38 +0900

    .

---
+++ Assets/02_Scripts/Network/RoomData.cs
@@ -0,0 +1,25 @@
+using Photon.Realtime;
+using TMPro;
+using UnityEngine;
+
+public class RoomData : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _roomCaption;
+
+    private RoomInfo _roomInfo;
+
+    // GetComponent<RoomData>().RoomInfo = room;
+
+    // Property
+    public RoomInfo RoomInfo
+    {
+        get => _roomInfo;
+        set
+        {
+            _roomInfo = value;
+            _roomCaption.text = $"{_roomInfo.Name} : ({_roomInfo.PlayerCount}/{_roomInfo.MaxPlayers})";
+        
+            // TODO: 버튼 클릭 이벤트 연결
+        }
+    }
+}
